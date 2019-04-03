1ec0078e37327ae15d2593c9ddf66d3f7b9e70e9
Glenn Canavan <gcana001@fiu.edu>
2019-04-03 03:04:53 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,38 +1,9 @@
-# Helper Methods
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+describe '#play' do
+  it 'calls turn nine times' do
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
+    allow(self).to receive(:gets).and_return('1','2','3','4','5','6','7','8','9')
+    
+    play(board)
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-# Define your play method below
+    expect(board).to eq(["X","X","X","X","X","X","X","X","X",])
